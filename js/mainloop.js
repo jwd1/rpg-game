2a34761803dc15d0e2d3a76f39486dc51c68e11f
John <johnwdriscoll1@gmail.com>
2016-09-05 17:12:52 +0100

    added reading nav buttons clicked able to move room to room

--- js/mainloop.js
+++ js/mainloop.js
@@ -9,3 +9,69 @@ var playerLuck = 0;
 init();
 displayPlayerStats();
 displayRoom();
+
+/* --------------------main -------------------------------------------------- */
+function reply_click(clicked_id) {
+    
+    if (clicked_id == "north_btn") {
+        
+        for (var i=0; i <rooms.length; i +=1) {
+            room = rooms[i];
+            if (playerLocation == room.location && room.north != "blocked") {
+                    playerLocation = room.north;
+                    displayRoom();
+                    return;
+            }
+
+        }
+    }
+
+    if (clicked_id == "west_btn") {
+        
+        for (var i=0; i <rooms.length; i +=1) {
+            room = rooms[i];
+            if (playerLocation == room.location && room.north != "blocked") {
+                    playerLocation = room.west;
+                    displayRoom();
+                    return;
+            }
+
+        }
+    }
+    
+    if (clicked_id == "east_btn") {
+        
+        for (var i=0; i <rooms.length; i +=1) {
+            room = rooms[i];
+            if (playerLocation == room.location && room.north != "blocked") {
+                    playerLocation = room.east;
+                    displayRoom();
+                    return;
+            }
+
+        }
+    }
+    
+    if (clicked_id == "south_btn") {
+        
+        for (var i=0; i <rooms.length; i +=1) {
+            room = rooms[i];
+            if (playerLocation == room.location && room.north != "blocked") {
+                    playerLocation = room.south;
+                    displayRoom();
+                    return;
+            }
+
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
